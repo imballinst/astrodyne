9e87455c4fc5f488e41e2b6affbc2cc6966127d2
Try Ajitiono <ballinst@gmail.com>
2023-02-19 14:13:03 +0700

    yep

--- packages/docgen/src/process-generated-api.ts
+++ packages/docgen/src/process-generated-api.ts
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import path from 'path';
+import { convertApiJSONToMarkdown } from './converters/converters';
 import {
   TopLevelFields,
   Child,
@@ -10,7 +11,7 @@ import { JSXType } from './models/_base';
 
 (async () => {
   const file = await fs.readFile(path.join(process.cwd(), 'api.json'), 'utf-8');
-  
+
   // Clean output folder.
   await Promise.allSettled([
     fs.rm('docs/components', { force: true }),
@@ -25,142 +26,10 @@ import { JSXType } from './models/_base';
   ]);
 
   const json = JSON.parse(file) as TopLevelFields;
-  const typeIdRecord: Record<number, Child> = {};
-
-  const componentsSection: Omit<RecordEntry, 'functions'>[] = [];
-  const functionsSection: Omit<RecordEntry, 'components'>[] = [];
-  const typesSection: Omit<RecordEntry, 'functions' | 'components'>[] = [];
-
-  for (const file of json.children) {
-    if (!file.children) {
-      continue;
-    }
-
-    const tempObj: RecordEntry = {
-      fileName: '',
-      components: {},
-      functions: {},
-      types: {}
-    };
-
-    for (const child of file.children) {
-      if (child.signatures && isJsxReturnType(child.signatures[0].type)) {
-        // Component: JSX.
-        tempObj.components[child.name] = child;
-      } else if (
-        child.kindString === 'Interface' ||
-        child.kindString === 'Type alias'
-      ) {
-        // Types.
-        tempObj.types[child.name] = child;
-        typeIdRecord[child.id] = child;
-      } else if (child.kindString === 'Function') {
-        // Functions.
-        tempObj.functions[child.name] = child;
-      } else if (child.kindString === 'Module') {
-        const moduleChildren = child.children || [];
-
-        for (const moduleChild of moduleChildren) {
-          tempObj.types[moduleChild.name] = moduleChild;
-          typeIdRecord[moduleChild.id] = moduleChild;
-        }
-      }
-    }
-
-    // Re-structure.
-    if (Object.keys(tempObj.components).length > 0) {
-      componentsSection.push({
-        fileName: file.name,
-        components: tempObj.components,
-        types: tempObj.types
-      });
-    } else if (Object.keys(tempObj.functions).length > 0) {
-      functionsSection.push({
-        fileName: file.name,
-        functions: tempObj.functions,
-        types: tempObj.types
-      });
-    } else {
-      typesSection.push({ fileName: file.name, types: tempObj.types });
-    }
-  }
-
-  const contents: string[][] = [];
-
-  // Components.
-  for (const section of componentsSection) {
-    const components: string[] = [];
-    const types: string[] = [];
-
-    for (const componentKey in section.components) {
-      const component = section.components[componentKey];
-
-      // TODO(imballinst): handle deprecated tags, etc.
-      components.push(
-        `
-### ${component.name}
-
-${(component.signatures![0].comment.summary || []).map((block) => block.text)}
-
-      `.trim()
-      );
-    }
-
-    types.push(...getTypeStringArray(section.types, typeIdRecord));
-
-    contents.push([
-      `docs/components/${path.basename(section.fileName)}.md`,
-      `
-## Components
-
-${components.join('\n\n')}
-
-## Types
-
-${types.join('\n\n')}
-    `.trim()
-    ]);
-  }
-
-  // Functions.
-  for (const section of functionsSection) {
-    const functions: string[] = [];
-    const types: string[] = [];
-
-    functions.push(...getFunctionStringArray(section.functions));
-    types.push(...getTypeStringArray(section.types, typeIdRecord));
-
-    contents.push([
-      `docs/functions/${path.basename(section.fileName)}.md`,
-      `
-## Functions
-
-${functions.join('\n\n')}
-
-## Types
-
-${types.join('\n\n')}
-        `.trim()
-    ]);
-  }
-
-  // Types.
-  for (const section of typesSection) {
-    const types: string[] = [];
-
-    types.push(...getTypeStringArray(section.types, typeIdRecord));
-    contents.push([
-      `docs/types/${path.basename(section.fileName)}.md`,
-      `
-## Types
-
-${types.join('\n\n')}
-        `.trim()
-    ]);
-  }
+  const contents = convertApiJSONToMarkdown(json);
 
   await Promise.allSettled(
-    contents.map(([filePath, content]) =>
+    Object.entries(contents).map(([filePath, content]) =>
       fs.writeFile(filePath, content, { encoding: 'utf-8' })
     )
   );

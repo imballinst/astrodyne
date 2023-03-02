9ce0ff3e6ce66fd186e4ba8df93560120da8cbf8
Try Ajitiono <ballinst@gmail.com>
2023-03-02 16:05:47 +0000

    test still failing, need to implement parameter blocks

--- packages/docgen/src/converters/converters.test.ts
+++ packages/docgen/src/converters/converters.test.ts
@@ -5,15 +5,16 @@ import * as path from 'path';
 import { convertApiJSONToMarkdown } from './converters';
 
 const apiJson = fs.readFileSync(
-  path.join(process.cwd(), 'example/api.json'),
+  path.join(process.cwd(), '../example/api.json'),
   'utf-8'
 );
 
-test('convertApiJSONToMarkdown', () => {
+describe('convertApiJSONToMarkdown', () => {
   const result = convertApiJSONToMarkdown(JSON.parse(apiJson));
 
-  expect(result['docs/components/TestComponent.md']).toBe(
-    `
+  test('components', () => {
+    expect(result['docs/components/TestComponent.md']).toBe(
+      `
 ## Components
 
 ### TestComponent
@@ -31,6 +32,35 @@ The props passed to TestComponent.
 | title | string | The title of the component. This is NOT the component ID. |
 | metadata | Metadata | The metadata of the component. |
 | information | Metadata | **[Deprecated]** Deprecated since 1.11.1. Please use \`metadata\` field instead as this might be<br/>removed in the future.<br/><br/>The component information. |
-  `.trim()
-  );
+    `.trim()
+    );
+  });
+
+  test('functions', () => {
+    expect(result['docs/functions/metadata.md']).toBe(
+      `
+## Functions
+
+### convertToMetadata
+
+Converts the information to metadata.
+
+#### Parameters
+
+| Prop | Type | Description |
+| ---- | ---- | ----------- |
+| information | object | The information object. |
+| information.id | object | The component ID. |
+| information.number | object | The component information's size. |
+
+#### Returns
+
+### getInformation
+
+Gets the component information from the given metadata.
+
+## Types
+    `.trim()
+    );
+  });
 });
--- packages/docgen/src/converters/converters.ts
+++ packages/docgen/src/converters/converters.ts
@@ -6,14 +6,12 @@ import {
   TopLevelFields
 } from '../models/models';
 import { JSXType } from '../models/_base';
-
-const TAG_TO_TAG_DESCRIPTION: Record<string, string> = {
-  '@deprecated': 'Deprecated'
-};
-enum NewlinePresentation {
-  LineBreak = '\n',
-  HTMLLineBreak = '<br/>'
-}
+import { getFunctionStringArray } from './function-converters';
+import {
+  getChildDescription,
+  getTypeBlock,
+  NewlinePresentation
+} from './type-converters';
 
 export function convertApiJSONToMarkdown(json: TopLevelFields) {
   const typeIdRecord: Record<number, Child> = {};
@@ -116,7 +114,7 @@ ${types.join('\n\n')}
     const functions: string[] = [];
     const types: string[] = [];
 
-    functions.push(...getFunctionStringArray(section.functions));
+    functions.push(...getFunctionStringArray(section.functions, typeIdRecord));
     types.push(...getTypeStringArray(section.types, typeIdRecord));
 
     const key = `docs/functions/${path.basename(section.fileName)}.md`;
@@ -157,17 +155,6 @@ function isJsxReturnType(type: ChildTypeUnion | undefined): type is JSXType {
   return type.type === 'reference' && type.name === 'Element';
 }
 
-function getFunctionStringArray(record: Record<string, Child>) {
-  return Object.values(record).map((fn) => {
-    return `
-### ${fn.name}
-
-${(fn.signatures![0].comment.summary || []).map((block) => block.text)}
-  
-    `.trim();
-  });
-}
-
 function getTypeStringArray(
   record: Record<string, Child>,
   typeIdRecord: Record<number, Child>
@@ -181,130 +168,4 @@ ${getChildDescription(type, NewlinePresentation.LineBreak)}
 ${getTypeBlock(type, typeIdRecord)}
     `.trim();
   });
-}
-
-function getTypeBlock(type: Child, typeIdRecord: Record<number, Child>) {
-  if (type.kindString === 'Type alias') {
-    return `
-\`\`\`ts
-type ${type.name} = ${getChildType(type, typeIdRecord)};\n
-\`\`\`
-  `.trim();
-  }
-
-  if (type.kindString === 'Interface') {
-    const { children = [] } = type;
-
-    return `
-| Prop | Type | Description |
-| ---- | ---- | ----------- |
-${processChildrenFields(children, typeIdRecord)}
-`.trim();
-  }
-
-  return '';
-}
-
-function processChildrenFields(
-  children: Child[],
-  typeIdRecord: Record<number, Child>
-) {
-  const rows: string[] = [];
-  children.sort((a, b) => a.id - b.id);
-
-  for (const child of children) {
-    // This only misses description, which we will extract from the tags below.
-    const row = [child.name, getChildType(child, typeIdRecord)];
-
-    row.push(getChildDescription(child, NewlinePresentation.HTMLLineBreak));
-    rows.push(`| ${row.join(' | ')} |`);
-  }
-
-  return rows.join('\n');
-}
-
-function getChildType(
-  child: Child,
-  typeIdRecord: Record<number, Child>
-): string {
-  if (child.type?.type === undefined) {
-    return '';
-  }
-
-  return `${getEffectiveType(child.type, child.name, typeIdRecord)}`;
-}
-
-function getEffectiveType(
-  type: ChildTypeUnion | undefined,
-  name: string,
-  typeIdRecord: Record<number, Child>
-): string {
-  if (type === undefined) return '';
-
-  if (type.type === 'array') {
-    return `${getEffectiveType(type.elementType, name, typeIdRecord)}[]`;
-  }
-
-  switch (type.type) {
-    case 'reference':
-    case 'intrinsic':
-      return type.name;
-    case 'literal':
-      return typeof type.value === 'string' ? `"${type.value}"` : type.value;
-    case 'union':
-      return (
-        '(' +
-        type.types
-          .map((inner) => getEffectiveType(inner, name, typeIdRecord))
-          .join(' | ') +
-        ')'
-      );
-    case 'reflection': {
-      const fields = (type.declaration.children || [])
-        .map((inner) => {
-          return `  ${inner.name}: ${getEffectiveType(
-            inner.type,
-            name,
-            typeIdRecord
-          )}`;
-        })
-        .join(';\n');
-
-      return `{\n${fields}\n}`;
-    }
-    default:
-      return '';
-  }
-}
-
-function getChildDescription(
-  child: Child,
-  newlineCharacter: NewlinePresentation
-) {
-  const tags = child.comment?.blockTags || [];
-  const summary = child.comment?.summary || [];
-  let description = '';
-
-  if (tags.length > 0) {
-    description += tags
-      .map((tag) => {
-        const description = tag.content
-          .map((block) => block.text.replace(/\n/g, newlineCharacter))
-          .join('');
-        const tagName = TAG_TO_TAG_DESCRIPTION[tag.tag]
-          ? `**[${TAG_TO_TAG_DESCRIPTION[tag.tag]}]** `
-          : '';
-
-        return `${tagName}${description}`;
-      })
-      .join('\n');
-  }
-
-  if (summary.length > 0) {
-    description += (summary || [])
-      .map((block) => block.text.replace(/\n/g, newlineCharacter))
-      .join('');
-  }
-
-  return description;
 }

a001da841bad4e07c50e7338d612083eb73b265e
Try Ajitiono <ballinst@gmail.com>
2023-04-08 05:39:09 +0000

    cp

--- packages/docgen/src/models/models.ts
+++ packages/docgen/src/models/models.ts
@@ -50,13 +50,13 @@ export const Signature = z
   .strict();
 export type Signature = z.infer<typeof Signature>;
 
-// TODO: we need to handle Record any many more TypeScript types -_- this is going to be PITA
 export const TypeScriptRecordType = z
   .object({
     type: z.literal('reference'),
     typeArguments: z.array(
       z.object({
         type: z.string(),
+        id: z.number().optional(),
         name: z.string()
       })
     ),
--- packages/example/src/packages/object/keys.ts
+++ packages/example/src/packages/object/keys.ts
@@ -1,3 +1,9 @@
+import { Metadata } from '../../helpers/types';
+
 export function getObjectKeys(object: Record<string, any>) {
   return Object.keys(object);
+}
+
+export function getMetadataValues(object: Record<string, Metadata>) {
+  return Object.values(object);
 }

6777e51e5f6bd07521b1643ab2c42e7535476b23
Try Ajitiono <ballinst@gmail.com>
2023-04-15 08:21:34 +0000

    alright, generic types seems like disaster

--- packages/example/src/packages/object/keys.ts
+++ packages/example/src/packages/object/keys.ts
@@ -1,5 +1,17 @@
 import { Metadata } from '../../helpers/types';
 
+type OmitNumberValues<T extends object> = {
+  [K in keyof T as T[K] extends number ? never : K]: T[K] extends number
+    ? never
+    : T[K];
+};
+
+interface ExampleValue {
+  field: string;
+  num: number;
+}
+export type ExampleValueWithoutNumber = OmitNumberValues<ExampleValue>;
+
 export function getObjectKeys(object: Record<string, any>) {
   return Object.keys(object);
 }
